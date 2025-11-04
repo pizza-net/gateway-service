aae1bd0b092e522e081643b2dd0e701313e1a783
Kacper Roman <rkacper132r@gmail.com>
2025-11-04 13:31:04 +0100

    test

--- src/main/java/com/pizzanet/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/pizzanet/gatewayservice/GatewayServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
-    //zmiana c
+    //zmiana c1
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
